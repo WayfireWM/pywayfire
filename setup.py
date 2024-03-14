482c7d3bd670bd2339d9647c71479afbe2182003
killown <systemofdown@gmai.com>
2024-03-14 07:30:56 -0300

    setup

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
 from setuptools import setup, find_packages
 
-setup(name="debus-python", packages=find_packages(include=["dbus-python"]))
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="wayfire",
+    version="1.0.0",
+    author="Killown",
+    author_email="systemofdown@gmail.com",
+    description="python module to control wayfire compositor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/killown/waypy",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
