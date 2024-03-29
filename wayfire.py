efdd1a8643585670f56239349e0a01f0a8cdb71b
killown <systemofdown@gmai.com>
2024-03-28 22:33:29 -0300

    more wayfire tests

--- wayfire.py
+++ wayfire.py
@@ -1456,7 +1456,7 @@ class WayfireSocket:
         functions = [
             (self.go_next_workspace_with_views, ()),
             (self.set_focused_view_to_workspace_without_views, ()),
-            # (self.test_move_cursor_and_click, ()),
+            (self.test_move_cursor_and_click, ()),
             (self.test_random_set_view_position, (view_id,)),
             (self.test_random_change_view_state, (view_id,)),
             (self.test_random_list_info, (view_id,)),
