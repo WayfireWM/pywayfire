130944b87906a29cd77c8f8eb909db81bbc4bdcd
killown <systemofdown@gmai.com>
2024-04-04 12:50:18 -0300

    removed test logs

--- wayfire/ipc.py
+++ wayfire/ipc.py
@@ -1847,13 +1847,13 @@ class WayfireSocket:
         thread = threading.Thread(target=spam_new_views)
         thread.start()
 
-        results_file = "/tmp/test-wayfire.log"
-        if os.path.exists(results_file):
-            with open(results_file, "w"):
-                pass
+        # results_file = "/tmp/test-wayfire.log"
+        # if os.path.exists(results_file):
+        #    with open(results_file, "w"):
+        #        pass
 
         # FIXME: Implement this to not use keybinds in the terminal with script running
-        first_view_focused = self.get_focused_view()
+        # first_view_focused = self.get_focused_view()
 
         # Execute functions with specified priority
         func_priority = self.test_set_function_priority(functions)
@@ -1869,13 +1869,13 @@ class WayfireSocket:
                 # Repeat certain functions every N iterations
                 if should_execute_function_priority > 10:
                     for func, args in func_priority:
-                        print(args)
-                        func(*args)
+                        result = func(*args)
+                        print(result)
                         # Log the function call with its arguments to the file
-                        with open(results_file, "a") as file:
-                            file.write(
-                                f"sock.{func.__name__}({', '.join(map(repr, args))})\n"
-                            )
+                        # with open(results_file, "a") as file:
+                        #    file.write(
+                        #        f"sock.{func.__name__}({', '.join(map(repr, args))})\n"
+                        #    )
                     should_execute_function_priority = 0
 
                 should_execute_function_priority += 1
@@ -1889,10 +1889,10 @@ class WayfireSocket:
                 random_function, args = choice(functions)
 
                 # Write the function call with "sock." prefix to the file
-                with open(results_file, "a") as file:
-                    file.write(
-                        f"sock.{random_function.__name__}({', '.join(map(repr, args))})\n"
-                    )
+                # with open(results_file, "a") as file:
+                #    file.write(
+                #        f"sock.{random_function.__name__}({', '.join(map(repr, args))})\n"
+                #    )
 
                 result = random_function(*args)
                 iterations += 1
