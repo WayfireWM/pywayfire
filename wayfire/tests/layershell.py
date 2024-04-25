ca121439b0a3f8f9ae8ddf0330ce8e632e49c20a
killown <systemofdown@gmai.com>
2024-04-24 22:02:16 -0300

    added layer shell lib import

--- wayfire/tests/layershell.py
+++ wayfire/tests/layershell.py
@@ -4,6 +4,10 @@ import time
 from wayfire.ipc import sock
 from random import randint
 
+
+from ctypes import CDLL
+CDLL("libgtk4-layer-shell.so")
+
 gi.require_version("Gtk", "3.0")
 gi.require_version("GtkLayerShell", "0.1")
 
