4b3e8b5cf6e13bb0f818c67d6b49a9e6b46abd36
killown <systemofdown@gmai.com>
2024-08-29 22:24:44 -0300

    remove duplicated functions

--- wayfire/extra/wpe.py
+++ wayfire/extra/wpe.py
@@ -5,24 +5,6 @@ class WPE:
     def __init__(self, socket: WayfireSocket):
         self.socket = socket
 
-    def filters_set_view_shader(self, view_id: int, shader: str):
-        message = get_msg_template("wf/filters/set-view-shader")
-        if message is None:
-            return
-        message["data"] = {}
-        message["data"]["view-id"] = view_id
-        message["data"]["shader-path"] = shader
-        return self.socket.send_json(message)
-
-    def filters_unset_view_shader(self, view_id: int):
-        message = get_msg_template("wf/filters/unset-view-shader")
-        if message is None:
-            return
-        message["data"] = {}
-        message["data"]["view-id"] = view_id
-        return self.socket.send_json(message)
-
-
     def set_view_opacity(self, view_id: int, opacity: float, duration: int):
         message = get_msg_template("wf/obs/set-view-opacity")
         message["data"] = {}
