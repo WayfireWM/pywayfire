1246430b0db9264f56034e979cf07710bc27927d
Scott Moreau <oreaus@gmail.com>
2024-09-02 23:29:32 -0600

    scripts: Add app-id and view id output to each event

--- scripts/event-monitor.py
+++ scripts/event-monitor.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+
+# A simple script to monitor wayfire ipc events.
+
 from wayfire import WayfireSocket
 
 sock = WayfireSocket()
@@ -7,6 +11,10 @@ while True:
     try:
         msg = sock.read_next_event()
         if "event" in msg:
-            print(msg)
+            print(msg["event"].ljust(25), end = ": ")
+            if "view" in msg:
+                if (msg["view"] is not None):
+                    print(msg["view"]["app-id"], end = " - ")
+                    print(msg["view"]["id"])
     except KeyboardInterrupt:
         exit(0)
