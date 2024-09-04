ddd902d74e0b6eaed862369e098612e6af38ffdc
killown <systemofdown@gmai.com>
2024-09-04 11:44:49 -0300

    removed run_wayfire function

--- wayfire/extra/ipc_utils.py
+++ wayfire/extra/ipc_utils.py
@@ -1,10 +1,4 @@
 from itertools import filterfalse
-from subprocess import Popen
-import os
-from uuid import uuid4
-from time import sleep
-from datetime import datetime, timezone
-from random import uniform
 from typing import Any, List, Optional, Tuple
 from wayfire import WayfireSocket
 from wayfire.extra.stipc import Stipc
@@ -89,66 +83,6 @@ class WayfireUtils:
             return True
         except OSError:
             return False
-
-    def run_wayfire(self, wayfire_path: str, logfile: str, cfgfile: str):
-        """
-        Start a Wayfire instance with a IPC connection.
-
-        Args:
-            wayfire_path (str): Path to the Wayfire executable.
-            logfile (str): Path to the file where logs will be written.
-            cfgfile (str): Path to the configuration file for Wayfire.
-
-        Returns:
-            A tuple containing: The Wayfire process, Display Name and IPC socket.
-
-        Example: process, display, socket = run_wayfire("/usr/bin/wayfire",
-                                                        "/path/to/wayfire.log",
-                                                        "/path/to/wayfire.ini")
-
-        Raises:
-            Exception: If unable to connect to the Wayfire IPC socket.
-        """
-        id = str(uuid4())
-        socket_name = f"/tmp/wayfire-{id}.socket"
-
-        env = os.environ.copy()
-        env['_WAYFIRE_SOCKET'] = socket_name
-
-        def get_now():
-           return datetime.now(tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
-
-        def get_display_name_from_log():
-            with open(logfile, "r") as log:
-                for line in log:
-                    if "Using socket name" in line:
-                        parts = line.split("Using socket name")
-                        if len(parts) > 1:
-                            display_name = parts[1].strip()
-                            return display_name
-                return None
-
-        with open(logfile, "w") as log:
-            log.write(f'Wayfire instance starting at {get_now()}\n')
-            log.flush()
-
-            # Run Wayfire with the generated socket name for IPC communication
-            wayfire_process = Popen(
-                [wayfire_path, '-c', cfgfile],
-                stdout=log, stderr=log, env=env, preexec_fn=os.setsid
-            )
-
-            sleep(1.5 + uniform(0, 1))
-
-            socket = WayfireSocket(socket_name)
-            display = get_display_name_from_log()
-
-            if not self.is_socket_active(socket):
-                log.write('Failed to connect to Wayfire IPC socket.\n')
-                log.flush()
-                raise Exception('Could not connect to Wayfire IPC socket.')
-
-            return wayfire_process, display, socket
 
     def move_view_to_empty_workspace(self, view_id: int) -> None:
         """
