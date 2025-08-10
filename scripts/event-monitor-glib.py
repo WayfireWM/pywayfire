8a28ae1b59decac3db46a2c4e019aa9afb566a2d
Scott Moreau <oreaus@gmail.com>
2025-08-10 07:38:10 -0600

    scripts: Scroll all the way to the bottom after adding event text

--- scripts/event-monitor-glib.py
+++ scripts/event-monitor-glib.py
@@ -19,19 +19,22 @@ class MyWindow(Gtk.ApplicationWindow):
         self.label.set_valign(Gtk.Align.START)
         self.scrolled_window.set_child(self.label)
         self.set_child(self.scrolled_window)
-
-        self.label_text = ""
+        vadjustment = self.scrolled_window.get_vadjustment()
+        vadjustment.connect("changed", self.on_vadjustment_changed)
 
         self.wf_socket = wayfire.WayfireSocket()
         self.wf_socket.watch()
         GLib.io_add_watch(self.wf_socket.client.fileno(), GLib.IO_IN, self.on_wf_event)
 
+    def idle_vadjustment_changed(self, vadjustment):
+        vadjustment.set_value(vadjustment.get_upper())
+
+    def on_vadjustment_changed(self, vadjustment):
+        GLib.idle_add(self.idle_vadjustment_changed, vadjustment)
+
     def on_wf_event(self, source, condition):
        if condition & GLib.IO_IN:
-           self.label_text += str(self.wf_socket.read_next_event()) + '\n'
-           self.label.set_label(self.label_text)
-           vadjustment = self.scrolled_window.get_vadjustment()
-           vadjustment.set_value(vadjustment.get_upper() - vadjustment.get_page_size())
+           self.label.set_label(self.label.get_text() + str(self.wf_socket.read_next_event()) + '\n')
        return True
 
 def on_activate(app):
